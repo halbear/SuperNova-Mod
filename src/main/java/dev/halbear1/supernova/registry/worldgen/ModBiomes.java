c5e8cefd358ae0103170ca94f5cd33121ae4400d
Hal <halbear.gamez@gmail.com>
2024-08-02 23:19:01 +0100

    space biome

--- src/main/java/dev/halbear1/supernova/registry/worldgen/ModBiomes.java
+++ src/main/java/dev/halbear1/supernova/registry/worldgen/ModBiomes.java
@@ -1,14 +1,38 @@
 package dev.halbear1.supernova.registry.worldgen;
 
 import dev.halbear1.supernova.SuperNova;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeMaker;
+import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class ModBiomes {
     public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, SuperNova.MOD_ID);
 
-    //space
+    static {
+        //space
+        createBiome("space", BiomeMaker::makeVoidBiome);
+    }
+
+    public static final RegistryKey<Biome> SPACE = registerBiome("space");
+
+    public static RegistryKey<Biome> registerBiome(String name) {
+        return RegistryKey.getOrCreateKey(Registry.BIOME_KEY, new ResourceLocation(SuperNova.MOD_ID, name));
+    }
+
+    public static RegistryObject<Biome> createBiome(String name, Supplier<Biome> biome) {
+        return BIOMES.register(name, biome);
+    }
+
+    public static void registerBiomes() {
+        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(SPACE, 1));
+    }
 
 }
--- src/main/java/dev/halbear1/supernova/registry/worldgen/ModSurfaceBuilders.java
+++ src/main/java/dev/halbear1/supernova/registry/worldgen/ModSurfaceBuilders.java
@@ -1,4 +1,11 @@
 package dev.halbear1.supernova.registry.worldgen;
 
+import dev.halbear1.supernova.SuperNova;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModSurfaceBuilders {
+    public static final DeferredRegister<SurfaceBuilder<?>> SURFACE_BUILDERS = DeferredRegister.create(ForgeRegistries.SURFACE_BUILDERS, SuperNova.MOD_ID);
+
 }
